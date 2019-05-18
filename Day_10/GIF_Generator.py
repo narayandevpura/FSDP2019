ce35e5afd5817bc8288c584a9316efd8d41a6f43
Narayan Devpura <narayandevpura@gmail.com>
2019-05-18 21:58:41 +0530

    Added new files

---
+++ Day_10/GIF_Generator.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 18 15:13:48 2019
+
+@author: Narayan Devpura
+"""
+
+"""
+
+Code Challenge 4
+GIF Creator
+
+A program that puts together multiple images (PNGs, JPGs, TIFFs) to make a smooth 
+GIF that can be exported. Make the program convert small video files to GIFs as 
+well.
+
+"""
---
+++ Day_10/Watermarking_system.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 18 15:07:06 2019
+
+@author: Narayan Devpura
+"""
+
+"""
+
+Code Challenge 3
+
+Watermarking Application
+
+Have some pictures you want copyright protected? Add your own logo or text lightly 
+across the background so that no one can simply steal your graphics off your site. 
+Make a program that will add this watermark to the picture.
+
+"""
+
