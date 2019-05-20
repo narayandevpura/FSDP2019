107d047978612213da5e46685f8dac951324131a
Narayan Devpura <narayandevpura@gmail.com>
2019-05-20 11:28:00 +0530

    GIF maker added

--- Day_10/GIF_Generator.py
+++ Day_10/GIF_Generator.py
@@ -15,3 +15,18 @@ GIF that can be exported. Make the program convert small video files to GIFs as
 well.
 
 """
+
+from PIL import Image
+images = []
+names=['apple.png','crown.jfif','egg.jfif','food.jfif','happy.jfif','what.jfif']
+for n in names:
+    frame = Image.open(n)
+    images.append(frame)
+
+# Save the frames as an animated GIF
+images[0].save('newgif.gif',
+               save_all=True,
+               append_images=images[1:],
+               duration=200,
+               loop=0)
+
